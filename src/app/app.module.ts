3ee7d57de07d00cb53efc84d3091da8dd8b1fee8
Mika Lehtinen <smibu90@gmail.com>
2022-07-17 16:06:01 +0300

    Update deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,6 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
